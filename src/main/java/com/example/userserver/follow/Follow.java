8d30bc1bb30fb1926719d079eb7d62855cf48fd5
joyj10 <micool1030@gmail.com>
2024-12-04 19:04:55 +0900

    [fix] JPA AuditingEntityListener 설정 추가

--- src/main/java/com/example/userserver/follow/Follow.java
+++ src/main/java/com/example/userserver/follow/Follow.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.ZonedDateTime;
 
@@ -11,6 +12,7 @@ import java.time.ZonedDateTime;
 @Table
 @Getter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Follow {
 
     @Id
